9fb7d5d8034c37fbb0d115e221b2249c3cb06755
ghostflyby <ghostflyby+git@outlook.com>
2025-03-31 14:50:50 +0800

    git init

---
+++ src/client/java/com/rcutanf/teamhunter/client/TeamhunterClient.java
@@ -0,0 +1,10 @@
+package com.rcutanf.teamhunter.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TeamhunterClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
