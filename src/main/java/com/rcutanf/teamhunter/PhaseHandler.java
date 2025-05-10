1bcbbc7ac8d199e23381d75f0b83f3b7658ba8db
RCUTANF <110910565+RCUTANF@users.noreply.github.com>
2025-05-11 02:10:23 +0800

    reset boarder size

--- src/main/java/com/rcutanf/teamhunter/PhaseHandler.java
+++ src/main/java/com/rcutanf/teamhunter/PhaseHandler.java
@@ -95,7 +95,7 @@ public class PhaseHandler {
 
         // 设置世界边界
         CommandExecutor.executeCommand(server, "/worldborder center 0 0");
-        CommandExecutor.executeCommand(server, "/worldborder set "+ TeamUtils.getMaxTeamPlayerCount(server)*50 ); // ±500*±500的边界
+        CommandExecutor.executeCommand(server, "/worldborder set "+ TeamUtils.getMaxTeamPlayerCount(server)*70 ); // ±500*±500的边界
 
 
         // 重置死亡统计
